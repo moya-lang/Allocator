ec3234c70307508a102b32df45aea8a34e24c7b9
moya-mmoczala <mmoczala@moya-lang.org>
2017-08-31 17:35:40 +0200

    Extracting MemoryPool to separate file

--- Allocator/Allocator.h
+++ Allocator/Allocator.h
@@ -3,86 +3,14 @@
 
 #include <memory>
 
+#include "MemoryPool.h"
+
 namespace Moya {
-
-template <class T, std::size_t growSize>
-class MemoryPool
-{
-    struct Block
-    {
-        Block *next;
-    };
-
-    class Buffer
-    {
-        static const std::size_t blockSize = sizeof(T) > sizeof(Block) ? sizeof(T) : sizeof(Block);
-        uint8_t data[blockSize * growSize];
-
-        public:
-
-            Buffer *const next;
-
-            Buffer(Buffer *next) :
-                next(next)
-            {
-            }
-
-            T *getBlock(std::size_t index)
-            {
-                return reinterpret_cast<T *>(&data[blockSize * index]);
-            }
-    };
-
-    Block *firstFreeBlock = nullptr;
-    Buffer *firstBuffer = nullptr;
-    std::size_t bufferedBlocks = growSize;
-
-
-    public:
-
-        ~MemoryPool()
-        {
-            while (firstBuffer) {
-                Buffer *buffer = firstBuffer;
-                firstBuffer = buffer->next;
-                delete buffer;
-            }
-        }
-
-        T *allocate()
-        {
-            if (firstFreeBlock) {
-                Block *block = firstFreeBlock;
-                firstFreeBlock = block->next;
-                return reinterpret_cast<T *>(block);
-            }
-
-            if (bufferedBlocks >= growSize) {
-                firstBuffer = new Buffer(firstBuffer);
-                bufferedBlocks = 0;
-            }
-
-            return firstBuffer->getBlock(bufferedBlocks++);
-        }
-
-        void deallocate(T *pointer)
-        {
-            Block *block = reinterpret_cast<Block *>(pointer);
-            block->next = firstFreeBlock;
-            firstFreeBlock = block;
-        }
-};
 
 template <class T, std::size_t growSize = 1024>
 class Allocator : private MemoryPool<T, growSize>
 {
     std::allocator<T> *defaultAllocator = nullptr;
-
-    Allocator(Allocator &&allocator);
-    Allocator(const Allocator &allocator);
-    Allocator &operator =(Allocator &&allocator);
-    Allocator &operator =(const Allocator &allocator);
-
 
     public:
 
